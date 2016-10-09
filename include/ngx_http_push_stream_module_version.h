4f4d8800c5ec8d81c84f138f098a57a6ac8d1141
Wandenberg <wandenberg@gmail.com>
2016-10-10 00:55:45 +0200

    add log message to version control

--- include/ngx_http_push_stream_module_version.h
+++ include/ngx_http_push_stream_module_version.h
@@ -26,7 +26,7 @@
 #ifndef NGX_HTTP_PUSH_STREAM_MODULE_VERSION_H_
 #define NGX_HTTP_PUSH_STREAM_MODULE_VERSION_H_
 
-static const ngx_str_t  NGX_HTTP_PUSH_STREAM_TAG = ngx_string("0.5.0");
-static const ngx_str_t  NGX_HTTP_PUSH_STREAM_COMMIT = ngx_string("5566c365977fbe4211b5c3757427b82604891a10");
+static const ngx_str_t  NGX_HTTP_PUSH_STREAM_TAG = ngx_string("0.5.2");
+static const ngx_str_t  NGX_HTTP_PUSH_STREAM_COMMIT = ngx_string("f3a02962363e55f4653952875f02da1e6ba293bb");
 
 #endif /* NGX_HTTP_PUSH_STREAM_MODULE_VERSION_H_ */
